a2daffcbfd33be972c4ba64800fa276122922305
Kateryna <k.borysenko.kyiv@gmail.com>
2022-06-16 16:21:07 +0300

    Added Deep copy function without using any third-party library in JS

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 //Task 1
 
+const makeObjectDeepCopy = (obj) => {
+  if (typeof obj === 'object') {
+    const cloneObj = {};
+    const objKeys = Object.keys(obj);
+    objKeys.forEach((key) => {
+      cloneObj[key] = makeObjectDeepCopy(obj[key]);
+    })
+    return cloneObj;
+  } else {
+    return obj;
+  }
+}
+
 //Task 2
 
 //Task 3
