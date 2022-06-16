a8ec6efac4d1a91142031a87a50855af85f8e001
Kateryna <k.borysenko.kyiv@gmail.com>
2022-06-16 17:35:14 +0300

    Added completed task 2

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //Task 1
-
 const makeObjectDeepCopy = (obj) => {
   if (typeof obj === 'object') {
     const cloneObj = {};
@@ -14,5 +13,27 @@ const makeObjectDeepCopy = (obj) => {
 }
 
 //Task 2
+const selectFromInterval = (arr, firstInt, secondInt) => {
+  if (!Array.isArray(arr)) {
+    throw new Error('First argument must be an array.');
+  }
+
+  const max = Math.max(firstInt, secondInt);
+  const min = Math.min(firstInt, secondInt);
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== 'number') {
+      throw new Error('Not a valid number.');
+    }
+    if (
+      arr[i] >= min &&
+      arr[i] <= max
+    ) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
 
 //Task 3
