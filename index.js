377af54eb51d788afa6cf582120ab78a17c74ba3
Kateryna <k.borysenko.kyiv@gmail.com>
2022-06-23 21:56:05 +0300

    Added task 2

--- index.js
+++ index.js
@@ -13,11 +13,68 @@ const concatStrings = (firstString = '', separator = '') => {
   }
 }
 
-concatStrings('first')('second')('third')()
-concatStrings('first', null)('second')()
-concatStrings('first', '123')('second')('third')()
-concatStrings('some-value')('')('')(null)
-concatStrings('some-value')(2)
-concatStrings('some-value')('333')(123n)
+concatStrings('first')('second')('third')();
+concatStrings('first', null)('second')();
+concatStrings('first', '123')('second')('third')();
+concatStrings('some-value')('')('')(null);
+concatStrings('some-value')(2);
+concatStrings('some-value')('333')(123n);
 
 //Task 2
+class Calculator {
+  constructor(firstNumber, secondNumber) {
+
+    if (arguments.length > 2) {
+      throw new Error('Must be two valid numbers');
+    }
+
+    this.setX(firstNumber);
+    this.setY(secondNumber);
+  }
+
+  isInvalidNumber(number) {
+    if (isNaN(number)) {
+      return true;
+    }
+  }
+
+  setX(number) {
+    if (this.isInvalidNumber(number)) {
+      throw new Error('The first number is not valid');
+    }
+
+    this.firstNumber = number;
+  }
+
+  setY(number) {
+    if (this.isInvalidNumber(number)) {
+      throw new Error('The second number is not valid');
+    }
+
+    this.secondNumber = number;
+  }
+
+  logSum = () => {
+    console.log(this.firstNumber + this.secondNumber);
+  }
+
+  logMul = () => {
+    console.log(this.firstNumber * this.secondNumber);
+  }
+
+  logSub = () => {
+    console.log(this.firstNumber - this.secondNumber);
+  }
+
+  logDiv = () => {
+    if (this.secondNumber === 0) {
+      throw new Error('Division by zero');
+    }
+
+    console.log(this.firstNumber / this.secondNumber);
+  }
+}
+
+const calculator = new Calculator(12, 3);
+calculator.logSum();
+calculator.logDiv(); 
