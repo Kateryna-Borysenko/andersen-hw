f2ce84d2d5a8e6c1819c3143db0878b0af91bf20
Kateryna <k.borysenko.kyiv@gmail.com>
2022-06-23 19:51:49 +0300

    Added task 1

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 //Task 1
+const concatStrings = (firstString = '', separator = '') => {
+  return (secondString) => {
+    if (typeof separator !== 'string') {
+      separator = '';
+    }
 
+    if (typeof secondString !== 'string') {
+      return `${firstString}`;
+    }
 
+    return concatStrings(`${firstString}${separator}${secondString}`, separator);
+  }
+}
+
+concatStrings('first')('second')('third')()
+concatStrings('first', null)('second')()
+concatStrings('first', '123')('second')('third')()
+concatStrings('some-value')('')('')(null)
+concatStrings('some-value')(2)
+concatStrings('some-value')('333')(123n)
 
 //Task 2
