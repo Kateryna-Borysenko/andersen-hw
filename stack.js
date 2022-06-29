c8a6920f8283b38df3f23c994de65b7be6107dd3
Kateryna <k.borysenko.kyiv@gmail.com>
2022-06-29 12:52:54 +0300

    added push and pop methods

--- stack.js
+++ stack.js
@@ -1,25 +1,58 @@
 class Stack {
-  constructor(size = 10) {
-    this.size = size;
-    this.list = list;
+  constructor(maxSize) {
+
+    if (isNaN(maxSize)) {
+      maxSize = 10;
+    }
+
+    this.size = maxSize;
+    this.data = [];
   }
 
   push(elem) {
+    if (this.data.length >= this.size) {
+      throw new Error('Stack Overflow');
+    }
+
+    return this.data = [...this.data, elem];
   }
 
   pop() {
+    if (this.data.length === 0) {
+      throw new Error('Stack is empty');
+    }
+
+    return this.data = [...this.data.slice(0, this.data.length - 1)];
   }
 
   peek() {
+    if (this.data.length === 0) {
+      return null;
+    }
+
+
   }
 
   isEmpty() {
+    return this.data.length === 0;
   }
 
   toArray() {
+
   }
 
   static fromIterable(iterable) {
-
   }
 }
+
+const stack = new Stack();
+stack.push('hello1')
+stack.push('hello2')
+stack.pop()
+// stack.pop()
+// stack.pop()
+
+console.log('🍒 stack', stack)
+
+
+module.exports = { Stack };
