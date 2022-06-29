0e05ef440373a9855b840fcc5430e3e2b719401a
Kateryna <k.borysenko.kyiv@gmail.com>
2022-06-29 11:13:45 +0300

    Added class stracture

---
+++ stack.js
@@ -0,0 +1,25 @@
+class Stack {
+  constructor(size = 10) {
+    this.size = size;
+    this.list = list;
+  }
+
+  push(elem) {
+  }
+
+  pop() {
+  }
+
+  peek() {
+  }
+
+  isEmpty() {
+  }
+
+  toArray() {
+  }
+
+  static fromIterable(iterable) {
+
+  }
+}
