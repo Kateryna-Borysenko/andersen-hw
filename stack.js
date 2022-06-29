0ec9b7bd04e54151f1e97d649fc4c5d6ad113af8
Kateryna <k.borysenko.kyiv@gmail.com>
2022-06-29 17:53:08 +0300

    Added completed task

--- stack.js
+++ stack.js
@@ -1,3 +1,111 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.top = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  append(value) {
+    const node = new Node(value);
+
+    if (!this.top) {
+      this.top = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+
+    this.size++;
+  }
+
+  prepend(value) {
+    const node = new Node(value);
+
+    if (!this.top) {
+      this.top = node;
+      this.tail = node;
+    } else {
+      this.top.prev = node;
+      node.next = this.top;
+      this.top = node;
+    }
+
+    this.size++;
+  }
+
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+
+    const node = this.tail;
+
+    if (this.size === 1) {
+      this.top = null;
+      this.tail = null;
+    } else {
+      this.tail = node.prev;
+      this.tail.next = null;
+      node.prev = null;
+    }
+
+    this.size--;
+
+    return node.value;
+  }
+
+  find(value) {
+    let current = this.top;
+
+    while (current) {
+      if (current.value === value) {
+        return current;
+      }
+
+      current = current.next;
+    }
+
+    return null;
+  }
+
+  toArray() {
+    const arr = [];
+    let current = this.top;
+
+    while (current) {
+      arr.push(current.value);
+      current = current.next;
+    }
+
+    return arr;
+  }
+
+  static fromIterable(iterable) {
+    if (iterable[Symbol.iterator] === undefined) {
+      throw new Error('Entity must be iterable');
+    }
+
+    const list = new LinkedList();
+
+    for (const elem of iterable) {
+      list.append(elem);
+    }
+
+    return list;
+  }
+}
+
+
 class Stack {
   constructor(maxSize) {
 
@@ -6,53 +114,54 @@ class Stack {
     }
 
     this.size = maxSize;
-    this.data = [];
+    this.list = new LinkedList();
   }
 
   push(elem) {
-    if (this.data.length >= this.size) {
+    if (this.list.length >= this.size) {
       throw new Error('Stack Overflow');
     }
 
-    return this.data = [...this.data, elem];
+    this.list.append(elem);
   }
 
   pop() {
-    if (this.data.length === 0) {
+    if (this.list.length === 0) {
       throw new Error('Stack is empty');
     }
 
-    return this.data = [...this.data.slice(0, this.data.length - 1)];
+    return this.list.pop();
   }
 
   peek() {
-    if (this.data.length === 0) {
+    if (this.list.length === 0) {
       return null;
     }
 
-
+    return this.list.tail.value;
   }
 
   isEmpty() {
-    return this.data.length === 0;
+    return this.list.length === 0;
   }
 
   toArray() {
-
+    return this.list.toArray();
   }
 
   static fromIterable(iterable) {
+    if (iterable[Symbol.iterator] === undefined) {
+      throw new Error('Entity must be iterable');
+    }
+
+    const stack = new Stack();
+
+    for (const elem of iterable) {
+      stack.push(elem);
+    }
+
+    return stack;
   }
 }
-
-const stack = new Stack();
-stack.push('hello1')
-stack.push('hello2')
-stack.pop()
-// stack.pop()
-// stack.pop()
-
-console.log('🍒 stack', stack)
-
 
 module.exports = { Stack };
